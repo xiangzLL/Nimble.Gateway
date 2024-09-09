c00b4cd345ce2d3da72569a8472edde98d6ffe2d
小邹 <751573533@qq.com>
2024-09-09 15:12:42 +0800

    Initial commit

---
+++ src/Boilerplate.Domain/Entities/Common/StronglyTypedIds.cs
@@ -0,0 +1,27 @@
+﻿using StronglyTypedIds;
+using System;
+
+[assembly: StronglyTypedIdDefaults(Template.Guid, "guid-efcore")]
+
+namespace Boilerplate.Domain.Entities.Common;
+
+
+public interface IGuid {}
+
+[StronglyTypedId]
+public partial struct HeroId : IGuid
+{
+    public static implicit operator HeroId(Guid guid)
+    {
+        return new HeroId(guid);
+    }
+}
+
+[StronglyTypedId]
+public partial struct UserId : IGuid
+{
+    public static implicit operator UserId(Guid guid)
+    {
+        return new UserId(guid);
+    }
+}
